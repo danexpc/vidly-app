fb7a9b3b1da2f5fcaa4aa398a5987cf2d5b7157f
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-09 08:01:35 +0300

    Pagination - Component Interface

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import Like from './common/like';
+import Pagination from './common/pagination';
 
 class Movies extends Component {
     state = { 
-        movies: getMovies()
+        movies: getMovies(),
+        pageSize: 4
     }
 
     handleDelete = id => {
@@ -27,6 +29,10 @@ class Movies extends Component {
                 movies
             }
         )
+    }
+
+    handlePageChange = page => {
+        console.log(page);
     }
 
     renderTable() {
@@ -80,6 +86,10 @@ class Movies extends Component {
                     { this.renderTable() }
                 </React.Fragment>
             )}
+            <Pagination 
+                itemsCount={movieCounter} 
+                pageSize={this.state.pageSize} 
+                onPageChange={this.handlePageChange}/>
             </React.Fragment>
         );
     }
