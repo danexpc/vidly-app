56c5495509f4f25d9ac1af841c259b248ab5c10f
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-12 10:19:53 +0300

    Sorting - Rendering Cell Content

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TableBody from './common/tableBody';
 import TableHeader from './common/tableHeader';
+import Like from './common/like';
 
 class MoviesTable extends Component {
   columns = [
@@ -8,12 +9,27 @@ class MoviesTable extends Component {
     { path: 'genre.name', label: 'Genre' },
     { path: 'numberInStock', label: 'Stock' },
     { path: 'dailyRentalRate', label: 'Rate' },
-    { key: 'like' },
-    { key: 'delete' },
+    {
+      key: 'like',
+      content: (movie) => (
+        <Like liked={movie.liked} onLike={() => this.props.onLike(movie)} />
+      ),
+    },
+    {
+      key: 'delete',
+      content: (movie) => (
+        <button
+          type='button'
+          className='btn btn-danger'
+          onClick={() => this.props.onDelete(movie._id)}>
+          Delete
+        </button>
+      ),
+    },
   ];
 
   render() {
-    const { movies, onDelete, onLike, onSort, sortColumn } = this.props;
+    const { movies, onSort, sortColumn } = this.props;
 
     return (
       <table className='table'>
@@ -23,27 +39,6 @@ class MoviesTable extends Component {
           onSort={onSort}
         />
         <TableBody data={movies} columns={this.columns} />
-        {/* <tbody>
-          {movies.map((movie) => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like liked={movie.liked} onLike={() => onLike(movie)} />
-              </td>
-              <td>
-                <button
-                  type='button'
-                  className='btn btn-danger'
-                  onClick={() => onDelete(movie._id)}>
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody> */}
       </table>
     );
   }
